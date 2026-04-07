62e146324befaa4e6267de8f3bb15cf7e79f56d0
lrizza <leonardorizza@hotmail.com>
2026-04-07 14:05:41 -0300

    1.5

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -6,13 +6,29 @@ export default function Layout() {
   return (
     <div className="flex flex-col min-h-screen">
       <Navbar />
-      <div className="w-full mt-20">
+
+      <div className="relative w-full mt-20 h-36 sm:h-48 md:h-80">
+
         <img
-          src="https://res.cloudinary.com/dxmydmsjl/image/upload/v1768300294/Tecnolog%C3%ADa_para_la_Integridad_y_los_Ensayos_No_Destructivos2_lba96j.png"
+          src="https://res.cloudinary.com/dxmydmsjl/image/upload/q_auto/f_auto/v1775580692/banner2_zitqwf.png"
           alt="Banner Vezta"
-          className="w-full h-36 sm:h-48 md:h-80 object-cover object-bottom-left"
+          className="absolute inset-0 w-full h-full object-cover object-left-middle"
         />
+
+        <div className="absolute inset-0 bg-white/70" />
+
+        <div className="absolute bottom-4 left-4 sm:bottom-6 sm:left-8 md:bottom-10 md:left-32 z-10">
+          <h1 className="text-2xl sm:text-4xl md:text-5xl lg:text-6xl font-black text-black drop-shadow-xl/50 tracking-wide">
+            VEZTA
+          </h1>
+          <p className="text-xs sm:text-sm md:text-lg lg:text-xl font-medium text-black drop-shadow-xl/50 mt-1">
+            Tecnología para la Integridad <br />
+            y los Ensayos No Destructivos
+          </p>
+        </div>
+
       </div>
+
       <main className="flex-grow">
         <Outlet />
       </main>
